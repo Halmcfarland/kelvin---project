dd3df4c7f09e4e64bb31e560bff222f75fabe215
Halmcfarland <168375917+Halmcfarland@users.noreply.github.com>
2024-04-30 19:41:01 -0500

    Create main.js

---
+++ main.js
@@ -0,0 +1,26 @@
+// Temperature forecast in Kelvin
+const kelvin = 293;
+
+// Temperature forecast in Celsius
+var celsius = kelvin - 273;
+
+// Temperature forecast in Fahrenheit
+var fahrenheit = celsius * (9/5) + 32;
+
+// Round Fahrenhiet temperature
+fahrenheit = Math.floor(fahrenheit);
+
+// Temperature forecast in Newton
+var newton = celsius * (33/100); 
+
+// Round Newton temperature
+newton = Math.floor(newton); 
+
+console.log('The temperature is ' + kelvin + ' degrees Kelvin.'
+);
+console.log('The temperature is ' + celsius + ' degrees Celsius.'
+);
+console.log('The temperature is ' + fahrenheit + ' degrees Fahrenheit.'
+);
+console.log('The temperature is ' + newton + ' degrees Newton.'
+);
